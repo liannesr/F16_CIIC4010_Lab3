b69542bf94957fea2504717a08c22a15a9a1e179
liannesr <lianne.sanchez@upr.edu>
2016-08-30 22:55:41 -0400

    Change of size, location, title and default on close operation.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -3,10 +3,15 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		
+		
 		JFrame myFrame = new JFrame("Lianne Sanchez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
 		myFrame.setLocation(850, 400);
+		
 		myFrame.setSize(200, 200);
+		
 		myFrame.setVisible(true);
+		
 	}
 }
