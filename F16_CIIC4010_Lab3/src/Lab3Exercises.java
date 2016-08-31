2475e2f7b6816b3e65078af1880a8933f0802e15
liannesr <lianne.sanchez@upr.edu>
2016-08-30 22:51:38 -0400

    Changed size, location and name of JFrame , also added default close operation.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
+		
 		JFrame myFrame = new JFrame("Lianne Sanchez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(850, 400);
